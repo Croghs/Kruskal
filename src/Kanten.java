db9d994fb9ea5637d80b27c1c631661d3e478891
ebietz <ebietz@gmx.de>
2018-05-13 16:55:00 +0200

    v1.0.3 - Auskommentiert und damit fertig

--- src/Kanten.java
+++ src/Kanten.java
@@ -1,12 +1,32 @@
+/**
+ * @author Altenhofen, Felix
+ * @author Bietz, Erik
+ * @author Lässig, Pauli
+ *
+ * OOP2 Aufgabe 08
+ * Implementierung der Kanten zwischen den Vertices
+ */
+
 public class Kanten implements Comparable<Kanten>{
     public int k1, k2, wert;
 
+    /**
+     * Konstruktor
+     * @param k1 Vertices 1
+     * @param k2 Vertices 2
+     * @param wert Wert der Kante
+     */
     public Kanten(int k1, int k2, int wert){
         this.k1 = k1;
         this.k2 = k2;
         this.wert = wert;
     }
 
+    /**
+     * Methode wird aus dem Comparable-Interface überschrieben, damit die Kanten miteinander verglichen werden können.
+     * @param o Kante, mit der verglichen werden soll
+     * @return 0, 1 oder -1 = gleich, größer, kleiner
+     */
     @Override
     public int compareTo(Kanten o) {
         Kanten e1 = (Kanten)o;
--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,12 +1,19 @@
-import java.lang.reflect.Array;
+/**
+ * @author Altenhofen, Felix
+ * @author Bietz, Erik
+ * @author Lässig, Pauli
+ *
+ * OOP2 Aufgabe 08
+ * Implementierung des Kruskal-Algorithmus
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class Kruskal {
 
 
-    final int anzahlKnoten = 7;
+    final int anzahlKnoten = 7;                     //Anzahl der Vertices im Baum
 
     Integer[] a = new Integer[anzahlKnoten];
     Integer[] m = new Integer[anzahlKnoten];
@@ -19,6 +26,9 @@ public class Kruskal {
         Kruskal kruskal = new Kruskal();
     }
 
+    /**
+     * Konstruktor, legt die Kanten zwischen den Knoten an
+     */
     public Kruskal(){
         q.add(new Kanten(0, 5, 5));
         q.add(new Kanten(0, 1, 8));
@@ -32,25 +42,30 @@ public class Kruskal {
         q.add(new Kanten(2, 6, 2));
         q.add(new Kanten(1, 2, 3));
         q.add(new Kanten(2, 3, 4));
-        Collections.sort(q);
-        ArrayList<Kanten> fertigerBaum = generateBaum(q);
-        output(fertigerBaum);
+        Collections.sort(q);                        //Kanten werden sortiert
+        ArrayList<Kanten> fertigerBaum = generateBaum(q);       //Baum wird erzeugt
+        output(fertigerBaum);                       //Baum wird ausgegeben
     }
 
+    /**
+     * Methode, die den fertigen Kruskal-Baum (minimal aufgespannten) erzeugt
+     * @param q sortierte Liste mit allen Kanten
+     * @return fertigen Baum
+     */
     public ArrayList<Kanten> generateBaum(ArrayList<Kanten> q){
         int min, max;
         ArrayList<Kanten> baum = new ArrayList<Kanten>();
-        for(int i = 0; i < anzahlKnoten; i++){
+        for(int i = 0; i < anzahlKnoten; i++){          //Hilfslisten werden befüllt
             a[i] = 1;
             m[i] = i;
             z[i] = i;
         }
 
         do{
-            Kanten k = q.remove(0);
+            Kanten k = q.remove(0);                 //niedrigste Kante wird bearbeitet
             int i = k.getK1();
             int j = k.getK2();
-            if(z[i] != z[j]){
+            if(z[i] != z[j]){                           //falls sie keinen Kreis bilden, wird sie hinzugefügt
                 baum.add(k);
                 if(a[z[i]] < a[z[j]]){
                     min = i;
@@ -69,12 +84,17 @@ public class Kruskal {
                 m[i] = m[j];
                 m[j] = temp;
             }
-        }while(baum.size() < anzahlKnoten-1);
-        return baum;
+        }while(baum.size() < anzahlKnoten-1);       //solange noch nicht alle Vertices miteinander verbunden sind
+        return baum;        //fertiger Baum wird zurückgegeben
     }
+
+    /**
+     * Gibt den Baum aus
+     * @param e fertiger Baum, der ausgegeben werden soll.
+     */
     public void output(ArrayList<Kanten> e){
         for(Kanten k : e){
-            System.out.println(k.getK1() + " - " + k.getWert() + " - " + k.getK2());
+            System.out.println(k.getK1() + " - " + k.getWert() + " - " + k.getK2());        //OUTPUT: Vertices1 - Wert - Vertices2
         }
     }
 
